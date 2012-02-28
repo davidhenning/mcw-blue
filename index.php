bd19ce3f0f700231abb5abbb089c8e6d89e7eada
MadCatme <madcat.me@gmail.com>
2012-02-28 22:56:08 +0100

    wp_head() in header, pagination in index

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 
 <div id="content">
 	<?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<?php get_template_part('post'); ?>
-	<?php endwhile; ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part('post'); ?>
+		<?php endwhile; ?>
+		<div id="pagination">
+			<div class="prev">
+				<?php previous_posts_link('&larr; Vorherige Eintr&auml;ge'); ?>&nbsp;
+			</div>
+			<div class="next">
+				&nbsp;<?php next_posts_link('N&auml;chste Eintr&auml;ge &rarr;',''); ?>
+			</div>
+		</div>
 	<?php endif; ?>
 </div>
 
