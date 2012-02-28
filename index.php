308502ce7dd745022f841d9eeee7b6eec48dfc11
MadCatme <madcat.me@gmail.com>
2012-02-28 22:45:57 +0100

    adding Wordpress stuff

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
+<?php get_header(); ?>
 
+<div id="content">
+	<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part('post'); ?>
+	<?php endwhile; ?>
+	<?php endif; ?>
+</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
