9af4c45f3eb9c71445d1f3d38d78279f2cf4f024
MadCatme <madcat.me@gmail.com>
2012-02-29 23:11:41 +0100

    added JS for external links and piwik tracking

--- footer.php
+++ footer.php
@@ -13,5 +13,22 @@
 			</div>
 		</footer>
 		<?php wp_footer(); ?>
+		<script type="text/javascript">
+			jQuery(document).ready(function() {
+				jQuery('article .content a:not([href*="madcatswelt"]):not([href*="d2a2u75mqv5jcj.cloudfront.net"])[href^="http"]:not([class*="noArrow"])').addClass('external').attr('title', 'Externer Link').attr('target', '_blank');
+			});
+		</script>
+		<!-- Piwik -->
+		<script type="text/javascript">
+		var pkBaseURL = (("https:" == document.location.protocol) ? "https://analytics.madcatswelt.org/" : "http://analytics.madcatswelt.org/");
+		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+		</script><script type="text/javascript">
+		try {
+		var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+		piwikTracker.trackPageView();
+		piwikTracker.enableLinkTracking();
+		} catch( err ) {}
+		</script><noscript><p><img src="http://analytics.madcatswelt.org/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+		<!-- End Piwik Tracking Code -->
 	</body>
 </html>
