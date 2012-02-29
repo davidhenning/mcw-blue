93542b5fe7c3ee4be7f969c2981121bec9cf78cb
MadCatme <madcat.me@gmail.com>
2012-02-29 22:19:26 +0100

    added highlighting for active item in dynamic site menu

--- functions.php
+++ functions.php
@@ -3,8 +3,18 @@
 if(function_exists('register_sidebar')) {
 	register_sidebar(array(
 		'before_widget' => '<ul><li id="%1$s" class="widget %2$s">',
-    'after_widget' => '</li></ul>',
-    'before_title' => '<h3 class="widgettitle">',
-    'after_title' => '</h3>',
+		'after_widget' => '</li></ul>',
+		'before_title' => '<h3 class="widgettitle">',
+		'after_title' => '</h3>',
 	));
+}
+
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class($classes, $item) {
+	if(is_home() && $item->title == 'weblog') { 
+		$classes[] = "current-menu-item";
+	}
+	
+	return $classes;
 }
