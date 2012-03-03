247d4671a851b42515aeb8b3735e2fc263bfa069
David Henning <madcat.me@gmail.com>
2012-03-04 00:26:03 +0100

    added function to get last quote from twitter

--- functions.php
+++ functions.php
@@ -18,3 +18,38 @@ function special_nav_class($classes, $item) {
 	
 	return $classes;
 }
+
+function getLastQuoteFromTwitter($sTwitterId = null) {
+	if($sTwitterId !== null) {
+		if(($aTweets = get_transient('twitter-quote')) === false) {
+			$aParams = array(
+				'screen_name'=> $sTwitterId, 
+				'trim_user'=> true, 
+				'include_entities'=> false, 
+				'include_rts' => true
+			);
+
+			$iTwitterTransientExpire = 900;
+			$sTwitterUrl = esc_url_raw('http://api.twitter.com/1/statuses/user_timeline.json?'.http_build_query($params), array('http'));
+			$aResponse = wp_remote_get($sTwitterUrl, array('User-Agent' => 'WordPress'));
+			$iResponseCode = wp_remote_retrieve_response_code($aResponse);
+			
+			if($iResponseCode == 200) {
+				$aTweets = json_decode(wp_remote_retrieve_body($aResponse), true);
+				
+				if(!is_array($aTweets) || isset($aTweets['error'])) {
+					$iTwitterTransientExpire = 300;
+					$aTweets = null;
+				}
+			}
+
+			set_transient('section-twitter', $aTweets, $iTwitterTransientExpire);
+
+			if($aTweets !== null) {
+				return $aTweets[0]['text'];
+			}
+		}
+	}
+
+	return null;
+}
