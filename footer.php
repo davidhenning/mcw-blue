7bb1ed112427f864d9e22bec861587b119ad7046
David Henning <madcat.me@gmail.com>
2012-03-04 00:42:05 +0100

    get last tweet for quoting

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
 			</div>
 		</div>
 		<footer>
-			<div id="tweet">
-				<div class="centerWrapper">
-					<q cite="http://twitter.com/#!/d_henning">Na klar, Besoffene rennen auf den Gleisen zwischen Ost- und Hauptbahnhof rum ...</q>
+			<?php if($sTweet = getLastQuoteFromTwitter('d_henning')): ?>
+				<div id="tweet">
+					<div class="centerWrapper">
+						<q cite="http://twitter.com/#!/d_henning"><?php echo $sTweet; ?></q>
+					</div>
 				</div>
-			</div>
+			<?php endif; ?>
 			<div id="info">
 				<div class="centerWrapper">
 					<a href="http://www.wordpress.org/" target="_blank">WordPress</a> | Theme: <a href="https://github.com/MadCatme/mcw-blue" target="_blank">mcw[blue]</a>
