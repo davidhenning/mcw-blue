74b8aad459ff40754c12f8ca6258081f94f84f71
David Henning <madcat.me@gmail.com>
2012-03-04 02:20:38 +0100

    fixed search

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 				<?php wp_nav_menu(array('container' => false)); ?>
 				<form action="<?php bloginfo('home'); ?>" method="get">
 					<fieldset id="search">
-						<input type="text" name="search" value="Suchen" onfocus="if (this.value == 'Suchen') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Suchen';}">
+						<input type="text" name="s" value="Suchen" onfocus="if (this.value == 'Suchen') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Suchen';}">
 						<input type="submit" name="search">
 					</fieldset>
 				</form>
