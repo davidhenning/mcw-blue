f1b29e7383103119c5d7011927a9b1b447063e21
David Henning <madcat.me@gmail.com>
2012-03-16 20:10:33 +0100

    added google analytics tracking (not yet tested)

---
+++ js/modules/tracking/googleanalytics.js
@@ -0,0 +1,17 @@
+define(function() {
+	var exports = {};
+
+	exports.init = function(element, trackingId) {
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', trackingId]);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	}
+
+	return exports;
+});
