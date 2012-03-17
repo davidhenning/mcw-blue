351d4b8a8fa5629414a66e0312917fde631f4d9b
David Henning <madcat.me@gmail.com>
2012-03-17 18:05:18 +0100

    use cdn url to load all modules

--- js/mcw.js
+++ js/mcw.js
@@ -1,11 +1,18 @@
-require.config({
+var cdnUrl = document.getElementsByTagName('body')[0].getAttribute('data-cdn-url');
+var requireConfig = {
   paths: {
     'jquery': 'libs/jquery/jquery-1.7.1',
     'exturls': 'modules/mcw/externalurls',
     'annotationloader': 'libs/core/annotationloader',
     'syntaxhl': 'modules/mcw/syntaxhl'
   }
-});
+};
+
+if(cdnUrl.length > 0 && cdnUrl.indexOf('http://') > -1) {
+  requireConfig.baseUrl = cdnUrl;
+}
+
+require.config(requireConfig);
 
 require(['jquery', 'exturls', 'annotationloader', 'syntaxhl'], function(jquery, exturls, annotationloader, syntaxhl){
   jquery.noConflict();
