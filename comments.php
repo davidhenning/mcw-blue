b04a453ef8a06dd78c86a5f396cfa5496ccf7a9c
MadCatme <madcat.me@gmail.com>
2012-03-20 23:58:00 +0100

    added missing language variables and provided german translation

--- comments.php
+++ comments.php
@@ -1,77 +1,96 @@
-<?php
-/**
- * The template for displaying Comments.
- *
- * The area of the page that contains both current comments
- * and the comment form. The actual display of comments is
- * handled by a callback to twentyeleven_comment() which is
- * located in the functions.php file.
- *
- * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
- */
-?>
-	<div id="comments">
-	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyeleven' ); ?></p>
-	</div><!-- #comments -->
-	<?php
-			/* Stop the rest of comments.php from being processed,
-			 * but don't kill the script entirely -- we still have
-			 * to fully load the template.
-			 */
-			return;
-		endif;
-	?>
+<div id="comments">
+    <?php if ( post_password_required() ) : ?>
+        <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'mcw-blue' ); ?></p>
+    </div><!-- #comments -->
+    <?php
+            return;
+        endif;
+    ?>
+    <?php if ( have_comments() ) : ?>
+        <h2 id="comments-title">
+            <?php
+                printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'mcw-blue' ),
+                    number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+            ?>
+        </h2>
 
-	<?php // You can start editing here -- including this comment! ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+            <nav id="comment-nav-above">
+                <h1 class="assistive-text"><?php _e( 'Comment navigation', 'mcw-blue' ); ?></h1>
+                <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'mcw-blue' ) ); ?></div>
+                <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'mcw-blue' ) ); ?></div>
+            </nav>
+        <?php endif;?>
 
-	<?php if ( have_comments() ) : ?>
-		<h2 id="comments-title">
-			<?php
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentyeleven' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
+        <ol>
+            <?php
+                wp_list_comments(array('callback' => function($comment, $args, $depth) { 
+                    $GLOBALS['comment'] = $comment;
+                ?>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above">
-			<h1 class="assistive-text"><?php _e( 'Comment navigation', 'twentyeleven' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentyeleven' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentyeleven' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
+                    <li>
+                        <article id="comment-<?php comment_ID(); ?>" class="comment">
+                            <header class="comment-meta">
+                                <div class="comment-author vcard">
+                                    <?php
+                                        $avatar_size = 68;
+                                        if ( '0' != $comment->comment_parent )
+                                            $avatar_size = 39;
 
-		<ol class="commentlist">
-			<?php
-				/* Loop through and list the comments. Tell wp_list_comments()
-				 * to use twentyeleven_comment() to format the comments.
-				 * If you want to overload this in a child theme then you can
-				 * define twentyeleven_comment() and that will be used instead.
-				 * See twentyeleven_comment() in twentyeleven/functions.php for more.
-				 */
-				wp_list_comments( array( 'callback' => 'twentyeleven_comment' ) );
-			?>
-		</ol>
+                                        echo get_avatar( $comment, $avatar_size );
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below">
-			<h1 class="assistive-text"><?php _e( 'Comment navigation', 'twentyeleven' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentyeleven' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentyeleven' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
+                                        printf( __( '%1$s on %2$s <span class="says">said:</span>', 'mcw-blue' ),
+                                            sprintf( '<span class="fn">%s</span>', get_comment_author_link() ),
+                                            sprintf( '<a href="%1$s"><time pubdate datetime="%2$s">%3$s</time></a>',
+                                                esc_url( get_comment_link( $comment->comment_ID ) ),
+                                                get_comment_time( 'c' ),
+                                                sprintf( __( '%1$s at %2$s', 'mcw-blue' ), get_comment_date(), get_comment_time() )
+                                            )
+                                        );
+                                    ?>
 
-	<?php
-		/* If there are no comments and comments are closed, let's leave a little note, shall we?
-		 * But we don't want the note on pages or post types that do not support comments.
-		 */
-		elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'twentyeleven' ); ?></p>
-	<?php endif; ?>
+                                    <?php edit_comment_link( __( 'Edit', 'mcw-blue' ), '<span class="edit">', '</span>' ); ?>
+                                </div>
 
-	<?php comment_form(); ?>
+                                <?php if ( $comment->comment_approved == '0' ) : ?>
+                                    <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'mcw-blue' ); ?></em>
+                                <?php endif; ?>
 
-</div><!-- #comments -->
+                            </header>
+                            <div class="content"><?php comment_text(); ?></div>
+                            <div class="reply">
+                                <?php 
+                                    comment_reply_link(array_merge($args, array(
+                                        'reply_text' => __( 'Reply <span>&darr;</span>', 'mcw-blue' ), 
+                                        'depth' => $depth, 
+                                        'max_depth' => $args['max_depth']
+                                    )));
+                                ?>
+                            </div>
+                        </article>
+                    </li>
+
+                <?php
+                }));
+            ?>
+        </ol>
+
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <nav id="comment-nav-below">
+            <h1 class="assistive-text"><?php _e( 'Comment navigation', 'mcw-blue' ); ?></h1>
+            <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'mcw-blue' ) ); ?></div>
+            <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'mcw-blue' ) ); ?></div>
+        </nav>
+        <?php endif; // check for comment navigation ?>
+
+    <?php
+        /* If there are no comments and comments are closed, let's leave a little note, shall we?
+         * But we don't want the note on pages or post types that do not support comments.
+         */
+        elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
+    ?>
+        <p class="nocomments"><?php _e( 'Comments are closed.', 'mcw-blue' ); ?></p>
+    <?php endif; ?>
+
+    <?php comment_form(); ?>
+</div>
