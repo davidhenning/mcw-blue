a4bd3635b43f4332f06acb320a2941713990991c
MadCatme <madcat.me@gmail.com>
2012-03-21 21:41:51 +0100

    minor changes in comment form

--- comments.php
+++ comments.php
@@ -94,5 +94,26 @@
         <p class="nocomments"><?php _e('Comments are closed.', 'mcw-blue'); ?></p>
     <?php endif; ?>
 
-    <?php comment_form(); ?>
+    <?php 
+        
+        $commenter = wp_get_current_commenter();
+        $req = get_option( 'require_name_email' );
+        $aria_req = ( $req ? " aria-required='true'" : '' );
+        $required_text = sprintf( ' ' . __('Required fields are marked %s'), '<span class="required">*</span>' );
+        $fields = array(
+            'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . ($req ? '<span class="required">*</span>' : '') . '</label> ' . 
+                        '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '"' . $aria_req . '></p>',
+            'email'  => '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label> ' . 
+                        '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '"' . $aria_req . '></p>',
+            'url'    => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label>' .
+                        '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '"></p>'
+        );
+      
+        comment_form(array(
+            'fields' => apply_filters('comment_form_default_fields', $fields),
+            'comment_notes_before' => '',      
+            'comment_notes_after' => '<p class="comment-notes">' . __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) . '</p>'
+        )); 
+
+    ?>
 </div>
