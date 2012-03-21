75687435fb06ea12e5b1766c73eb420b7f77acef
MadCatme <madcat.me@gmail.com>
2012-03-21 20:45:59 +0100

    better source code formatting

--- comments.php
+++ comments.php
@@ -1,24 +1,24 @@
 <div id="comments">
-    <?php if ( post_password_required() ) : ?>
-        <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'mcw-blue' ); ?></p>
-    </div><!-- #comments -->
+    <?php if(post_password_required()) : ?>
+        <p class="nopassword"><?php _e('This post is password protected. Enter the password to view any comments.', 'mcw-blue'); ?></p>
+    </div>
     <?php
             return;
         endif;
     ?>
-    <?php if ( have_comments() ) : ?>
-        <h2 id="comments-title">
+    <?php if(have_comments()): ?>
+        <h2>
             <?php
                 printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'mcw-blue' ),
-                    number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+                    number_format_i18n(get_comments_number()), get_the_title());
             ?>
         </h2>
 
-        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <?php if(get_comment_pages_count() > 1 && get_option('page_comments')): ?>
             <nav id="comment-nav-above">
-                <h1 class="assistive-text"><?php _e( 'Comment navigation', 'mcw-blue' ); ?></h1>
-                <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'mcw-blue' ) ); ?></div>
-                <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'mcw-blue' ) ); ?></div>
+                <h1 class="assistive-text"><?php _e('Comment navigation', 'mcw-blue'); ?></h1>
+                <div class="nav-previous"><?php previous_comments_link( __('&larr; Older Comments', 'mcw-blue')); ?></div>
+                <div class="nav-next"><?php next_comments_link(__('Newer Comments &rarr;', 'mcw-blue')); ?></div>
             </nav>
         <?php endif;?>
 
@@ -34,26 +34,27 @@
                                 <div class="comment-author vcard">
                                     <?php
                                         $avatar_size = 68;
-                                        if ( '0' != $comment->comment_parent )
+                                        if($comment->comment_parent != '0') {
                                             $avatar_size = 39;
+                                        }   
+                                            
+                                        echo get_avatar($comment, $avatar_size);
 
-                                        echo get_avatar( $comment, $avatar_size );
-
-                                        printf( __( '%1$s on %2$s <span class="says">said:</span>', 'mcw-blue' ),
-                                            sprintf( '<span class="fn">%s</span>', get_comment_author_link() ),
-                                            sprintf( '<a href="%1$s"><time pubdate datetime="%2$s">%3$s</time></a>',
-                                                esc_url( get_comment_link( $comment->comment_ID ) ),
-                                                get_comment_time( 'c' ),
-                                                sprintf( __( '%1$s at %2$s', 'mcw-blue' ), get_comment_date(), get_comment_time() )
+                                        printf(__('%1$s on %2$s <span class="says">said:</span>', 'mcw-blue'),
+                                            sprintf('<span class="fn">%s</span>', get_comment_author_link()),
+                                            sprintf('<a href="%1$s"><time pubdate datetime="%2$s">%3$s</time></a>',
+                                                esc_url(get_comment_link( $comment->comment_ID)),
+                                                get_comment_time('c'),
+                                                sprintf(__('%1$s at %2$s', 'mcw-blue'), get_comment_date(), get_comment_time())
                                             )
                                         );
                                     ?>
 
-                                    <?php edit_comment_link( __( 'Edit', 'mcw-blue' ), '<span class="edit">', '</span>' ); ?>
+                                    <?php edit_comment_link(__('Edit', 'mcw-blue' ), '<span class="edit">', '</span>'); ?>
                                 </div>
 
-                                <?php if ( $comment->comment_approved == '0' ) : ?>
-                                    <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'mcw-blue' ); ?></em>
+                                <?php if($comment->comment_approved == '0'): ?>
+                                    <em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.', 'mcw-blue'); ?></em>
                                 <?php endif; ?>
 
                             </header>
@@ -61,7 +62,7 @@
                             <div class="reply">
                                 <?php 
                                     comment_reply_link(array_merge($args, array(
-                                        'reply_text' => __( 'Reply <span>&darr;</span>', 'mcw-blue' ), 
+                                        'reply_text' => __('Reply <span>&darr;</span>', 'mcw-blue'), 
                                         'depth' => $depth, 
                                         'max_depth' => $args['max_depth']
                                     )));
@@ -75,21 +76,18 @@
             ?>
         </ol>
 
-        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <?php if(get_comment_pages_count() > 1 && get_option('page_comments')): ?>
         <nav id="comment-nav-below">
-            <h1 class="assistive-text"><?php _e( 'Comment navigation', 'mcw-blue' ); ?></h1>
-            <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'mcw-blue' ) ); ?></div>
-            <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'mcw-blue' ) ); ?></div>
+            <h1 class="assistive-text"><?php _e('Comment navigation', 'mcw-blue'); ?></h1>
+            <div class="nav-previous"><?php previous_comments_link(__('&larr; Older Comments', 'mcw-blue')); ?></div>
+            <div class="nav-next"><?php next_comments_link(__('Newer Comments &rarr;', 'mcw-blue')); ?></div>
         </nav>
-        <?php endif; // check for comment navigation ?>
+        <?php endif; ?>
 
     <?php
-        /* If there are no comments and comments are closed, let's leave a little note, shall we?
-         * But we don't want the note on pages or post types that do not support comments.
-         */
-        elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
+        elseif (! comments_open() && ! is_page() && post_type_supports(get_post_type(), 'comments')):
     ?>
-        <p class="nocomments"><?php _e( 'Comments are closed.', 'mcw-blue' ); ?></p>
+        <p class="nocomments"><?php _e('Comments are closed.', 'mcw-blue'); ?></p>
     <?php endif; ?>
 
     <?php comment_form(); ?>
